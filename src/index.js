cc5fb18fcb05df89aa2ec8f66a397120ba73c952
minwoowow <jmwoo1127@gmail.com>
2024-11-06 02:19:53 +0900

    Modify: link시 스크롤 위치 수정

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { BrowserRouter } from "react-router-dom";
+import ScrollTop from './ScrollTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollTop />
       <App />
-    </BrowserRouter>        
+    </BrowserRouter>
   </React.StrictMode>
 );
